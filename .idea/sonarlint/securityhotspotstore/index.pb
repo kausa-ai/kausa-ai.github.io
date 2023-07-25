ce1b6e3367779a73260a4aa4f01106572d62a633
Pinak Bheed <pinak@kausa.ai>
2023-07-25 09:00:28 +0200

    wget static-website dump

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,4 @@
+
+:
+
+about.html,8/1/8143af03f594989ae71e2d1be57beadf78357d4c
---
+++ _next/static/4DdXKgYOG8lCjkD3iryJ7/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblog","\u002Fearly-access","\u002F","\u002Farticle\u002F[slug]","\u002Fblog\u002F[slug]","\u002F[...slug]","\u002Fcase-study\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
